8ace6251dab1568527243e76135cd7bac834efae
Diana Oros <diana@dianas-macbook-air.local>
2019-03-07 18:21:12 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Diana Oros on 2/22/19.
+//  Copyright © 2019 Diana Oros. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
